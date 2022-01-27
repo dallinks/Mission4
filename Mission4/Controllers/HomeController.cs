f4442d8be89e965aa303d7289c2ffbef0be41e37
dallinks <dallinstirling@gmail.com>
2022-01-26 23:51:19 -0700

    Add project files.

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private NewMovieContext _NewMovieContextFile { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, NewMovieContext someName)
+        {
+            _logger = logger;
+            _NewMovieContextFile = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public IActionResult AddNewMovie()
+        {
+            return View("NewMovie");
+        }
+        [HttpPost]
+        public IActionResult AddNewMovie(ApplicationResponse response)
+        {
+            _NewMovieContextFile.Add(response);
+            _NewMovieContextFile.SaveChanges();
+            return View("Confirmation",response);
+        }
+    }
+}
---
+++ Mission4/Models/NewMovieContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mission4.Models
+{
+    public class NewMovieContext : DbContext
+    {
+        public NewMovieContext (DbContextOptions<NewMovieContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<ApplicationResponse> responses { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    MovieID = 1,
+                    Category = "Action",
+                    Title = "Naruto",
+                    Year = 1994,
+                    Director = "John Smith",
+                    Rating = "G",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+                );
+        }
+    }
+}
